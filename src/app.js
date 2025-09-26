1db9ec6d643f57ddc095e5cd5569c963e8338d92
toufiqulislamtanmoy <tittanmoy@gmail.com>
2025-09-26 11:46:57 +0600

    deploy server on render

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.send("Hello from NPL Backend!"));
 
 app.use("/api/auth", authRoutes);
 // Global error handler
